d32b92d1808bf58aaa3a47bf690da42102fa9937
EnzoViry1 <eviry@kuzzle.io>
2026-06-02 09:22:23 +0200

    feat(base provider): add support attachement property

--- lib/providers/BaseProvider.ts
+++ lib/providers/BaseProvider.ts
@@ -34,6 +34,8 @@ export abstract class BaseProvider<T> {
 
   protected accounts = new Map<string, T>();
 
+  public supportAttachment: boolean = false;
+
   protected EVENT_ACCOUNT_ADD: string;
   protected EVENT_ACCOUNT_REMOVE: string;
 
